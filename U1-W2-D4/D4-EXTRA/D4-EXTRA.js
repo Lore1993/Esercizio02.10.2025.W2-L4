3c8b4db8618318ab772fbec9214747a2de6952e1
Lore1993 <lorenzo93pellegrini@gmail.com>
2025-10-04 13:22:14 +0200

    Update D4-EXTRA.js

--- U1-W2-D4/D4-EXTRA/D4-EXTRA.js
+++ U1-W2-D4/D4-EXTRA/D4-EXTRA.js
@@ -7,7 +7,19 @@
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+const giveMeRandom = function (a,b) {
+    let somma = a+b
+    let random = Math.floor(Math.random() *10)+1;
+    return somma + random
+ }
+const checkArray = function (giveMeRandom){
+    if (giveMeRandom >5){console.log("Valore maggiore di 5")}
+else {console.log("Minore di 5")} }
 
+let risultato = giveMeRandom(2, 1) // 
+console.log("Risultato:", risultato)
+
+checkArray(risultato) 
 
 /* EXTRA 2
  Nel tuo eCommerce disponi di un'array di oggetti chiamato "shoppingCart". Ognuno di questi oggetti ha le seguenti proprietà: "price", "name", "id" e "quantity".
@@ -15,13 +27,54 @@
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+const shoppingCart = [
+  { id: 1, name: "Laptop", price: 1000, quantity: 1 },
+  { id: 2, name: "Mouse", price: 25, quantity: 2 },
+  { id: 3, name: "Monitor", price: 200, quantity: 1 },
+];
+
+const shoppingCartTotal = function () {
+    let totale = 0
+  for (let i = 0; i < shoppingCart.length; i++) {
+    totale += shoppingCart[i].price * shoppingCart[i].quantity;
+  }
+  return totale
+}
+console.log("Totale carrello:", shoppingCartTotal(shoppingCart));
 
 /* EXTRA 3
  Nel tuo eCommerce disponi di un'array di oggetti chiamato "shoppingCart". Ognuno di questi oggetti ha le seguenti proprietà: "price", "name", "id" e "quantity".
  Crea una funzione chiamata "addToShoppingCart" che riceve un nuovo oggetto dello stesso tipo, lo aggiunge a "shoppingCart" e ritorna il nuovo numero totale degli elementi.
 */
 
+
+// Funzione per aggiungere un prodotto
+function addToShoppingCart(newItem) {
+
+    // Calcolo il numero totale di elementi (somma di tutte le quantità)
+    let totalItems = 0;
+     const existingItem = shoppingCart.find(item => item.id === newItem.id)
+    if (existingItem) {
+        // Se esiste, aumento la quantità
+        existingItem.quantity += newItem.quantity;
+    } else {
+        // Altrimenti lo aggiungo come nuovo oggetto
+        shoppingCart.push(newItem);
+ 
+    }
+
+    // Ritorno il totale
+    return totalItems;
+}
+const nuovoProdotto = { id: 4, name: "Keyboard", price: 150, quantity: 1 };
+console.log(addToShoppingCart(nuovoProdotto)); // 10
+console.log(shoppingCart);
+const nuovoProdotto1 = { id: 2, name: "Mouse", price: 25, quantity: 1 };
+console.log(addToShoppingCart(nuovoProdotto1)); // Aggiorna la quantità
+console.log(shoppingCart);
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+
 
 /* EXTRA 4
  Nel tuo eCommerce disponi di un'array di oggetti chiamato "shoppingCart". Ognuno di questi oggetti ha le seguenti proprietà: "price", "name", "id" e "quantity".
